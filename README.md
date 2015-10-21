a8c490b2e39fc04d0c7b9d73a04c358c1b4ceb05
Jason W. May <jmay@pobox.com>
2015-10-21 14:53:39 -0700

    notes

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Implementation of a simple site-specific command-line scraper.
 
 All the real work is delegated to the MetaInspector gem.
 
+Given a URL, it will fetch that URL and all immediate internal links
+from that page. Output is a JSON-formatted summary of the internal
+links and (internal or external) assets (images & stylesheets) used on
+each page.
+
 ## Installation
 
 ```ruby
@@ -15,3 +20,46 @@ git clone http://github.com/jmay/myscrape
 ```ruby
 myscrape/bin/scrape http://example.com/
 ```
+
+To avoid pounding sites with lots of links, the scraper by default
+will only pull 3 sub-pages.
+
+## Comments
+
+Prompt was as follows:
+
+> Write a web crawler in a language of your choice. It should be limited
+> to one domain - so when crawling opusforwork.com it would crawl all
+> pages within the opusforwork.com domain, but not follow any outside
+> links. Given a URL, it should output a site map, showing which static
+> assets each page depends on, and the links between pages. Choose the
+> most appropriate data structure to store & display this site map e.g.
+> printing it to stdout or writing it to a file.
+
+> Build this as you would something for production. Focus on code
+> quality and write tests as appropriate.  Make sure to include a README
+> documenting how you laid out the code and why you designed it the way
+> you did.
+
+Web crawling/scraping is well-known territory. There's rarely a reason
+to build a new one from scratch, so I looked for a decent open-source
+implementation to work from. There's an excellent Python package
+called [Scrapy](http://scrapy.org) but that looked like overkill, and
+I'm more familiar with Ruby.
+
+MetaInspector is much lighter than Scrapy, but is actively under
+development with [some outside contributions](https://github.com/jaimeiniesta/metainspector/graphs/contributors).
+
+Using an existing gem means I could rely on the gem for unit testing
+and worry only about integration testing for the specific use case
+described here. See =spec/myscrape_spec.rb= for test cases.
+
+Some of the more recent frameworks can conceal content (such as
+images) from scrapers by using Javascript lazy or incremental
+page-loading techniques. I've not attempted to deal with these. See
+http://brettterpstra.com or https://meta.discourse.org for examples:
+images that appear on the page do not appear in the HTML source
+retrieved by the scraper.
+
+So, a simple gem that wraps MetaInspector and a command-line
+executable in `bin/scrape` that runs the most common case.
--- spec/myscrape_spec.rb
+++ spec/myscrape_spec.rb
@@ -57,4 +57,73 @@ describe Myscrape do
                                       }                                    ])
   end
 
+  it 'scrapes stanfordhealthcare root' do
+    scraper = Myscrape::Scraper.new('https://stanfordhealthcare.org')
+    expect(scraper.summary).to eq(
+                                   {
+                                     url: "https://stanfordhealthcare.org/",
+                                     links: [
+                                       "/for-patients-visitors/locations-and-parking.html",
+                                       "/for-patients-visitors/visiting-hours.html",
+                                       "/for-patients-visitors/your-hospital-stay.html",
+                                       "/for-patients-visitors/international-services.html",
+                                       "/for-patients-visitors/billing.html",
+                                       "/for-patients-visitors/support-groups.html",
+                                       "/for-patients-visitors.html",
+                                       "/health-care-professionals/referring-physicians.html",
+                                       "/health-care-professionals/nursing.html",
+                                       "/health-care-professionals/allied-healthcare.html",
+                                       "/health-care-professionals.html",
+                                       "/health-care-professionals/referring-physicians/prism.html",
+                                       "/campaigns/stroke-neuroimaging.html",
+                                       "/medical-clinics/primary-care.html",
+                                       "/campaigns/breast-cancer-awareness-month.html",
+                                       "/campaigns/atrial-fibrillation-firm.html",
+                                       "/medical-clinics/cardiac-arrhythmia-service.html",
+                                       "/stanford-health-now/videos/new-mapping-technology-target-afib.html",
+                                       "/campaigns/stanford-cancer-center-south-bay-open.html",
+                                       "/medical-clinics/south-bay-cancer-center.html",
+                                       "/newsroom/articles/2015/shc-completes-sccsb.html",
+                                       "/campaigns/select-your-doctor.html",
+                                       "/search-results.conditions.html",
+                                       "/search-results.clinics.html",
+                                       "/search-results.doctors.html",
+                                       "/stanford-health-now.html",
+                                       "/stanford-health-now.html/.html",
+                                       "/newsroom/articles/2015/shc-ranks-among-nations-most-connected-hospitals.html",
+                                       "/newsroom/articles/2015/stanford-health-care-ceo-step-down-2015.html",
+                                       "/stanfordhealthcare.html/",
+                                       "/for-patients-visitors/volunteering.html",
+                                       "/about-us.html",
+                                       "/newsroom.html",
+                                       "/about-us/legal-disclaimer.html",
+                                       "/for-patients-visitors/your-hospital-stay/patient-privacy.html",
+                                       "/about-us/conduct.html",
+                                       "/newsroom/listings.html"
+                                     ],
+                                     assets: [
+                                       "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700",
+                                       "https://stanfordhealthcare.org/etc/clientlibs/shc/aem-optimized.min.d41d8cd98f00b204e9800998ecf8427e.css",
+                                       "https://stanfordhealthcare.org/etc/clientlibs/shc/main.min.3274a7e36c0638a5538029f859174615.css",
+                                       "https://stanfordhealthcare.org/etc/clientlibs/shc/aggregate.megasearch.min.css",
+                                       "https://stanfordhealthcare.org/etc/clientlibs/shc/aggregateapps.min.eadfbc1ae9128b5d281297224fc85cc9.css",
+                                       "https://stanfordhealthcare.org/etc/clientlibs/shc/main/img/logo-shc.png",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/Campaign/stroke-neuroimaging/images/stroke-721-444-tm.jpg.transform/721x444/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/Campaign/breast-cancer-awareness/bca-ribbon.jpg.transform/221x221/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/Campaign/Atrial-Fibrillation/images/narayan-721-444-tm.jpg.transform/721x444/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/clinics/locations/300pasteurdr-stanford.jpg.transform/221x221/q82/image.jpg",
+                                       "https://img.youtube.com/vi/1r5nx-cav3o/maxresdefault.jpg",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/Campaign/sccsb/sccsb-crawford-homepage.jpg.transform/721x444/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/Campaign/sccsb/sccsb-new-building.jpg.transform/221x221/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/shc/en/newsroom/articles/2015/shc-completes-sccsb/_jcr_content/squareThumbnail.transform/221x221/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/dam/SHC/health-care-professionals/healthcareprofessionals-photo-mdstethoscope.jpg.transform/original/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/shc/en/newsroom/articles/2015/shc-ranks-among-nations-most-connected-hospitals/_jcr_content/squareThumbnail.transform/100x100/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/shc/en/_jcr_content/home-main-parsys/general_container_0/parsyscontainer/content_box/personImage1.transform/original/q82/image.png",
+                                       "https://stanfordhealthcare.org/content/shc/en/_jcr_content/home-main-parsys/mvp_promotion/desktopImage.transform/300x260/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/shc/en/_jcr_content/home-main-parsys/mvp_promotion/desktopImage2.transform/300x260/q82/image.jpg",
+                                       "https://stanfordhealthcare.org/content/shc/en/_jcr_content/home-main-parsys/mvp_promotion/desktopImage3.transform/300x260/q82/image.jpg"
+                                     ]
+                                   }
+                               )
+  end
 end
